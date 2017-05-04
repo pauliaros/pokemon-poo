10499cda6d3e613d42ba46c63f09efa07c7792e9
Paulina Aros <aros.pau@gmail.com>
2017-05-04 14:01:53 -0300

    se agrega ejercicio 1

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -15,7 +15,7 @@ function Pokemon(nombre,color,poderDeAtaque){
 		this.nivelDeAmistad = this.nivelDeAmistad + valor
 	}
 	this.atacar = function(pokemon){
-		pokemon.vida = pokemon.vida - this.poderDeAtaque
+		return pokemon.vida = pokemon.vida - this.poderDeAtaque
 	}
 }
 
@@ -24,3 +24,27 @@ const Charmander = new Pokemon ("Charmander", "rojo", 20)
 
 Pikachu.atacar(Charmander)
 console.log(Charmander.vida)
+
+
+function peleaPokemon(){
+	var pokeUno = document.getElementById("pokemon-uno").value;
+	var pokeDos = document.getElementById("pokemon-dos").value;
+
+	if(pokeUno == pokeDos){
+		alert("No puedes hacer pelear dos pokemones iguales")
+		return;
+	}
+
+	var ataquePokeUno = prompt("Ingresa poder de ataque del primer Pokemon");
+	var ataquePokeDos = prompt("Ingresa poder de ataque del segundo Pokemon");
+
+	var pimerPoke = new Pokemon (pokeUno,"amarillo",ataquePokeUno)
+	var segundoPoke = new Pokemon (pokeDos,"rojo",ataquePokeDos)
+	
+	var ataque = pimerPoke.atacar(pokeDos);
+	var etiquetaFinal = document.getElementById("resultado");
+	var textoFinal = pokeUno + " ataco a " + pokeDos + " y " +  pokeDos + " tiene una vida de : " + segundoPoke.vida ;	
+
+	etiquetaFinal.innerHTML = textoFinal;
+
+}
